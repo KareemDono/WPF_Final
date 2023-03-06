45eb10d66db29ab8282ea7a3c381204690cc24a2
KareemDono <99328665+KareemDono@users.noreply.github.com>
2023-03-06 09:00:13 +0200

    Add project files.

---
+++ final smester 1 project/MainWindow.xaml.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Net;
+using System.Net.Mail;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace final_smester_1_project
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const string regexPattern = @"^[a-zA-Z0-9\.\-_]+@[a-zA-Z]{2,15}(?:\.[a-zA-Z]+){1,2}$";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataLoadGrid();
+        }
+
+        //sql data base
+        SqlConnection connection = new SqlConnection(@"Data Source=SNOOPDOGG\SQLEXPRESS;Initial Catalog=WPFDataBase;Integrated Security=True");
+
+        //loading database table from sql
+        public void DataLoadGrid()
+        {
+            SqlCommand cmd = new SqlCommand("select * from LoginTable", connection);
+            DataTable dt = new DataTable();
+            connection.Open();
+            SqlDataReader sdr = cmd.ExecuteReader();
+            dt.Load(sdr);
+            connection.Close();
+            datagrid.ItemsSource = dt.DefaultView;
+        }
+
+        //checking if the text boxes are not empty or does not match required input -- sign up
+        public bool CheckInputSignUp()
+        {
+            if (firstNameSignUp.Text == String.Empty)
+            {
+                MessageBox.Show("First Name is required", "Failed", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            else if (!Regex.IsMatch(firstNameSignUp.Text, @"^[a-zA-Z]+$"))
+            {
+                MessageBox.Show("First Name should contain only letters", "Failed", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+
+            if (lastNameSignUp.Text == String.Empty)
+            {
+                MessageBox.Show("Last Name is required", "Failed", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            else if (!Regex.IsMatch(lastNameSignUp.Text, @"^[a-zA-Z]+$"))
+            {
+                MessageBox.Show("Last Name should contain only letters", "Failed", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+
+            if (emailSignUp.Text == String.Empty)
+            {
+                MessageBox.Show("Email is required", "Failed", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            else if (!Regex.IsMatch(emailSignUp.Text, @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$"))
+            {
+                MessageBox.Show("Email should contain 'example1-9@domaina.com'", "Failed", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+
+            if (passwordSignUp.Password == String.Empty)
+            {
+                MessageBox.Show("Password is required", "Failed", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            else if (!Regex.IsMatch(passwordSignUp.Password, @"^.{1,16}$"))
+            {
+                MessageBox.Show("Password should contain 16 chars max", "Failed", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+
+            return true;
+        }
+
+        //email send function
+        private void SendEmail(object sender, RoutedEventArgs e)
+        {
+            //tayeb cpehdgmxpignhodn
+            //kareem ifuqkdvohbpreqcr
+
+            string from = fromEmail.Text;
+            string fromPassword = passwordEmail.Password;
+            string to = toEmail.Text;
+            string subject1 = subjectEmail.Text;
+            string body1 = contentEmail.Text;
+
+            try
+            {
+                if (!Regex.IsMatch(from, regexPattern) || !Regex.IsMatch(to, regexPattern))
+                {
+                    MessageBox.Show("Email is incorrect");
+                    return;
+                }
+
+                MailMessage message = new MailMessage(from, to, subject1, body1);
+                SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+                smtp.EnableSsl = true;
+                smtp.Credentials = new NetworkCredential(from, fromPassword);
+                smtp.Send(message);
+                MessageBox.Show("Send successfully");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        //sign up function that insert the values of the params to the sql data
+        private void SignUpButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (CheckInputSignUp())
+                {
+                    SHA256 sha256 = SHA256.Create();
+                    byte[] hashBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(passwordSignUp.Password));
+                    string passwordHash = Convert.ToBase64String(hashBytes);
+                    SqlCommand cmd = new SqlCommand("INSERT INTO LoginTable VALUES (@FirstName, @LastName, @Email, @Password)", connection);
+                    cmd.CommandType = CommandType.Text;
+                    cmd.Parameters.AddWithValue("@FirstName", firstNameSignUp.Text);
+                    cmd.Parameters.AddWithValue("@LastName", lastNameSignUp.Text);
+                    cmd.Parameters.AddWithValue("@Email", emailSignUp.Text);
+                    cmd.Parameters.AddWithValue("@Password", passwordHash);
+                    connection.Open();
+                    cmd.ExecuteNonQuery();
+                    connection.Close();
+                    DataLoadGrid();
+                    MessageBox.Show("Registered Successfully", "Saved", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        //sign in function
+        private void SignInButton_Click(object sender, RoutedEventArgs e)
+        {
+            connection.Open();
+
+            SHA256 sha256 = SHA256.Create();
+            byte[] hashBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(passwordSignIn.Password));
+            string passwordHash = Convert.ToBase64String(hashBytes);
+
+            string query = $"SELECT * FROM LoginTable WHERE Email='{emailSignIn.Text}' AND Password='{passwordHash}'";
+
+            SqlCommand sqlCommand = new SqlCommand(query, connection);
+            SqlDataReader reader = sqlCommand.ExecuteReader();
+
+            if (reader.Read())
+            {
+                MessageBox.Show("Signed In Successfully");
+                RefresWindow();
+                reader.Close();
+            }
+            else
+            {
+                MessageBox.Show("Signed In Failed");
+            }
+
+            connection.Close();
+        }
+
+        //refresh the window to reset the inputs
+        public void RefresWindow()
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            Close();
+        }
+
+        //clear fields
+        public void Clear()
+        {
+            firstNameSignUp.Clear();
+            lastNameSignUp.Clear();
+            emailSignUp.Clear();
+            passwordSignUp.Clear();
+        }
+
+        //delte the stored valus from the sql data
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            connection.Open();
+            SqlCommand cmd = new SqlCommand("delete from LoginTable", connection);
+            try
+            {
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Table Has Been Deleted", "Deleted", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+            finally
+            {
+                connection.Close();
+                Clear();
+                DataLoadGrid();
+            }
+        }
+
+
+        private void redirectButton_Click(object sender, RoutedEventArgs e)
+        {
+            StoredProcedure storedProcedure = new StoredProcedure();
+            storedProcedure.Show();
+        }
+
+        private void firstNameSignUp_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void lastNameSignUp_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void emailSignUp_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void passwordSignUp_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void emailSignIn_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void passwordSignIn_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+
+
+        private void fromEmail_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void toEmail_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void subjectEmail_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void contentEmail_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ClearDataGrid_Button(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        
+    }
+}
---
+++ final smester 1 project/StoredProcedure.xaml.cs
@@ -0,0 +1,318 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace final_smester_1_project
+{
+    /// <summary>
+    /// Interaction logic for StoredProcedure.xaml
+    /// </summary>
+    public partial class StoredProcedure : Window
+    {
+        private SqlConnection connection;
+        private SqlCommand cmd;
+
+        public StoredProcedure()
+        {
+            InitializeComponent();
+
+            connection = new SqlConnection(@"Data Source=SNOOPDOGG\SQLEXPRESS;Initial Catalog=WPFDataBase;Integrated Security=True");
+            cmd = new SqlCommand();
+            cmd.Connection = connection;
+
+            LoadData();
+        }
+
+        
+        //display the values of the params by the id that the user insert
+        private void btnSelect_Click(object sender, RoutedEventArgs e)
+        {
+            int employeeID;
+            if (int.TryParse(txtEmployeeID.Text, out employeeID))
+            {
+                cmd.CommandText = "SELECT * FROM Employee WHERE EmployeeID = @EmployeeID";
+                cmd.CommandType = CommandType.Text;
+                cmd.Parameters.Clear();
+                cmd.Parameters.AddWithValue("@EmployeeID", employeeID);
+
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+
+                try
+                {
+                    connection.Open();
+                    adapter.Fill(dt);
+                }
+                catch (SqlException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+
+                if (dt.Rows.Count > 0)
+                {
+                    txtEmployeeName.Text = dt.Rows[0]["EmployeeName"].ToString();
+                    txtEmployeeEmail.Text = dt.Rows[0]["EmployeeEmail"].ToString();
+                    txtEmployeeAge.Text = dt.Rows[0]["EmployeeAge"].ToString();
+                    txtEmployeeCity.Text = dt.Rows[0]["EmployeeCity"].ToString();
+                }
+                else
+                {
+                    MessageBox.Show("Employee not found.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Employee ID must be an integer.");
+            }
+        }
+
+        //delete the values of the params from the sql data
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            int employeeID;
+            if (int.TryParse(txtEmployeeID.Text, out employeeID))
+            {
+                cmd.CommandText = "DELETE FROM Employee WHERE EmployeeID = @EmployeeID";
+                cmd.CommandType = CommandType.Text;
+                cmd.Parameters.Clear();
+                cmd.Parameters.AddWithValue("@EmployeeID", employeeID);
+
+                try
+                {
+                    connection.Open();
+                    cmd.ExecuteNonQuery();
+                }
+                catch (SqlException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+
+                LoadData();
+            }
+            else
+            {
+                MessageBox.Show("Employee ID must be an integer.");
+            }
+        }
+
+        //insert the user inputs to the sql data
+        private void btnInsert_Click(object sender, RoutedEventArgs e)
+        {
+            int employeeID;
+            int employeeAge;
+            if (!int.TryParse(txtEmployeeID.Text, out employeeID))
+            {
+                MessageBox.Show("ID is required and should only contain numbers", "Failed", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            if (!int.TryParse(txtEmployeeAge.Text, out employeeAge))
+            {
+                MessageBox.Show("Age is required and should only contain numbers", "Failed", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            if (CheckUserInput())
+            {
+                string employeeName = txtEmployeeName.Text;
+                string employeeEmail = txtEmployeeEmail.Text;
+                string employeeCity = txtEmployeeCity.Text;
+
+                cmd.CommandText = "dbo.Employee_Insert";
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Clear();
+                cmd.Parameters.AddWithValue("@EmployeeID", employeeID);
+                cmd.Parameters.AddWithValue("@EmployeeName", employeeName);
+                cmd.Parameters.AddWithValue("@EmployeeEmail", employeeEmail);
+                cmd.Parameters.AddWithValue("@EmployeeAge", employeeAge);
+                cmd.Parameters.AddWithValue("@EmployeeCity", employeeCity);
+
+                try
+                {
+                    connection.Open();
+                    cmd.ExecuteNonQuery();
+                }
+                catch (SqlException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+
+                LoadData();
+            }
+        }
+
+        //update the user inputs to the sql data
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            int employeeID;
+            int employeeAge;
+            if (!int.TryParse(txtEmployeeID.Text, out employeeID))
+            {
+                MessageBox.Show("ID is required and should only contain numbers", "Failed", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            if (!int.TryParse(txtEmployeeAge.Text, out employeeAge))
+            {
+                MessageBox.Show("Age is required and should only contain numbers", "Failed", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            if (CheckUserInput())
+            {
+                string employeeName = txtEmployeeName.Text;
+                string employeeEmail = txtEmployeeEmail.Text;
+                string employeeCity = txtEmployeeCity.Text;
+
+                cmd.CommandText = "UPDATE Employee SET EmployeeName = @EmployeeName, EmployeeEmail = @EmployeeEmail, EmployeeAge = @EmployeeAge, EmployeeCity = @EmployeeCity WHERE EmployeeID = @EmployeeID";
+                cmd.CommandType = CommandType.Text;
+                cmd.Parameters.Clear();
+                cmd.Parameters.AddWithValue("@EmployeeID", employeeID);
+                cmd.Parameters.AddWithValue("@EmployeeName", employeeName);
+                cmd.Parameters.AddWithValue("@EmployeeEmail", employeeEmail);
+                cmd.Parameters.AddWithValue("@EmployeeAge", employeeAge);
+                cmd.Parameters.AddWithValue("@EmployeeCity", employeeCity);
+
+                try
+                {
+                    connection.Open();
+                    cmd.ExecuteNonQuery();
+                }
+                catch (SqlException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+
+                LoadData();
+            }
+        }
+
+        //load function
+        private void LoadData()
+        {
+            cmd.CommandText = "SELECT * FROM Employee";
+            cmd.CommandType = CommandType.Text;
+
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+
+            try
+            {
+                connection.Open();
+                adapter.Fill(dt);
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            dgEmployees.ItemsSource = dt.DefaultView;
+        }
+
+        //checking if the text boxes are not empty or does not match required input
+        public bool CheckUserInput()
+        {
+            if (txtEmployeeName.Text == String.Empty)
+            {
+                MessageBox.Show("Name is required", "Failed", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            else if (!Regex.IsMatch(txtEmployeeName.Text, @"^[a-zA-Z]+$"))
+            {
+                MessageBox.Show("Name should contain only letters", "Failed", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+
+            if (txtEmployeeEmail.Text == String.Empty)
+            {
+                MessageBox.Show("Email is required", "Failed", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            else if (!Regex.IsMatch(txtEmployeeEmail.Text, @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$"))
+            {
+                MessageBox.Show("Email should contain 'example1-9@domaina.com'", "Failed", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+
+            if (txtEmployeeCity.Text == String.Empty)
+            {
+                MessageBox.Show("City is required", "Failed", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            else if (!Regex.IsMatch(txtEmployeeCity.Text, @"^[a-zA-Z]+$"))
+            {
+                MessageBox.Show("City should contain only letters", "Failed", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+
+            return true;
+        }
+
+        //redirect the main window
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+        }
+
+        private void txtEmployeeEmail_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void txtEmployeeName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void txtEmployeeID_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void dgEmployees_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void txtEmployeeAge_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void txtEmployeeCity_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+       
+    }
+}
